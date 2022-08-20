583e0002c1634b370f10bca83a90b0b67c15dd6f
Milton <miltonsoftwareengineer@gmail.com>
2022-08-20 18:38:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+
+const num1= 31
+const num2= 2
+const multiply = (num1*num2)
+ 
+const random = Math.floor(Math.random()*10)
+
+const num3= 19
+const num4= 5
+const mod =(num3 % num4 )
+
+const set = [12, 14, 16, 20]
+const max = Math.max(... set)
+
+
+// const chai = require('chai')
+// const expect = chai.expect;
+
+
+//       expect(mod).to.eq(4)
+//     });
+//   });
+//   describe('max', function() {
+//     it("will return 20 as the highest number in the set", function() {
+//       expect(max).to.eq(20)
+//     });
+//   });
+// });
